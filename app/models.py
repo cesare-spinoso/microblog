bc0b5b220582aec67ede1458f27ac29bf8f895a4
cesare-spinoso <cesare.spinoso@gmail.com>
2020-07-06 15:30:40 -0400

    Finished ch 6

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+# For the profile avatar gravatar.com
+# Since the avatar belongs to the user, the logic is implemented here
+from hashlib import md5
 
 
 # Create the user table as a class
@@ -13,6 +16,10 @@ class User(UserMixin, db.Model):  # Inherits from db.Model base class for all mo
     posts = db.relationship('Post', backref = 'author', lazy='dynamic')
     # Post is the raltion in the many side, backref is a field added to many class that points to one,
     # lazy how is the DB query issued
+
+    # Add more info to user profile page. Include description and last active
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -30,12 +37,19 @@ class User(UserMixin, db.Model):  # Inherits from db.Model base class for all mo
     # is_autheticated, is_active, is_anonymous, get_id()
     # Since these are tedious to implement use mixin class to do it for you
 
+    # Implement the avatar logic
+    def avatar(self, size):
+        # This method will return the url to gavatar to grab the avatar
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 # This load_user function is used by Flask-Login to remember the user navigating through different pages
 # It uses the id of the user which is passed as a string by Flask-Login (that's why use a cast to int)
+
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 # Create the post table as a class, it has a foreign key to user 1 user can have many posts
 # but a post has a single user
--- readme.txt
+++ readme.txt
@@ -1,2 +1,6 @@
-Continue from Chapter 4 Database:
+Continue from Chapter 6 User Page:
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+
+Note:
+username: admin
+password: root
